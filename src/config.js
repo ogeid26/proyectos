a16d824143e65e70c36b8bad04c8ab0f8ae4148d
ogeid26 <dmalvaradog26@gmail.com>
2024-11-08 16:47:57 -0300

    first commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export const metaData = {
+  baseUrl: "https://astrofolio-astro.vercel.app/",
+  title: "mis proyectos y protitipos",
+  name: "Vikas",
+  ogImage: "/opengraph-image.png",
+  description:
+    "Una lista (no exhaustiva) de los proyectos que he hecho o en los que he participado.",
+};
+
+export const socialLinks = {
+  twitter: "https://x.com/vikas5914",
+  github: "https://github.com/vikas5914/Astrofolio",
+  instagram: "https://www.instagram.com/",
+  linkedin: "https://www.linkedin.com/",
+  email: "mailto:example@gmail.com",
+};
