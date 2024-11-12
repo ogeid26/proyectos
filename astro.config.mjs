bfc7745677b584265fcf54fc8df5b939c4f963b2
ogeid26 <dmalvaradog26@gmail.com>
2024-11-12 15:09:08 -0300

    WORKING

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,84 +1,76 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import vercel from "@astrojs/vercel/static";
 import icon from "astro-icon";
 import opengraphImages, { presets } from "astro-opengraph-images";
-import astroI18next from "astro-i18next";
+
+import tailwind from "@astrojs/tailwind";
+import astroI18next  from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astrofolio-astro.vercel.app",
   integrations: [
-    astroI18next(),
+   astroI18next(), 
     mdx({
-      image: {
-        domains: ["unsplash.com"],
-      },
-      optimize: true,
-      shikiConfig: {
-        themes: {
-          light: "github-light",
-          dark: "github-dark",
-          langs: [],
-        },
-      },
-      remarkPlugins: [remarkMath],
-      rehypePlugins: [
-        rehypeSlug,
-        [
-          rehypeAutolinkHeadings,
-          {
-            behavior: "prepend",
-            properties: {
-              className: "anchor",
-            },
-          },
-        ],
-        rehypeKatex,
-      ],
-      gfm: true,
-    }),
-    sitemap(),
-
-    react({
-      experimentalReactChildren: true,
-    }),
-    icon({
-      include: {
-        "fa6-solid": ["rss", "circle-half-stroke"],
-        tabler: ["mail-filled"],
-        "fa6-brands": ["x-twitter", "github", "instagram", "linkedin-in"],
-      },
-    }),
-    sitemap(),
-    opengraphImages({
-      render: presets.waveSvg,
-      options: {
-        fonts: [
-          {
-            name: "Roboto",
-            name: "Roboto",
-            name: "Roboto",
-            weight: 400,
-            style: "normal",
-            
-          },
-        ],
-      },
-    }),
-  ],
-  output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
+    image: {
+      domains: ["unsplash.com"],
     },
-    imageService: true,
-    devImageService: "sharp",
-  }),
+    optimize: true,
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+        langs: [],
+      },
+    },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "prepend",
+          properties: {
+            className: "anchor",
+          },
+        },
+      ],
+      rehypeKatex,
+    ],
+    gfm: true,
+  }), sitemap(), 
+  tailwind(),
+  react({
+    experimentalReactChildren: true,
+  }), icon({
+    include: {
+      "fa6-solid": ["rss", "circle-half-stroke"],
+      tabler: ["mail-filled"],
+      "fa6-brands": ["x-twitter", "github", "instagram", "linkedin-in"],
+    },
+  }), sitemap(), opengraphImages({
+    render: presets.waveSvg,
+    options: {
+      fonts: [
+        {
+          name: "Roboto",
+          name: "Roboto",
+          name: "Roboto",
+          weight: 400,
+          style: "normal",
+          // data: fs.readFileSync(
+          //   "node_modules/@fontsource/roboto/files/roboto-latin-400-normal.woff"
+          // ),
+        },
+      ],
+    },
+  }), tailwind()],
+  output: "static",
 });
